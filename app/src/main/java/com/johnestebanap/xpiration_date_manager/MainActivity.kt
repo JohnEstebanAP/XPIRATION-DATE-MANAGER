44364890e59b27ab6df2f6b3ef498357d8868580
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-04-19 12:47:39 -0500

    Initial commit

---
+++ app/src/main/java/com/johnestebanap/xpiration_date_manager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.johnestebanap.xpiration_date_manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
